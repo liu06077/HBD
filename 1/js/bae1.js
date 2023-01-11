ad632a24c5d4e0e75463e44f76e4be616acab8b9
liu06077 <119809547+liu06077@users.noreply.github.com>
2023-01-11 12:15:24 +0800

    b1

---
+++ 1/js/bae1.js
@@ -0,0 +1,28 @@
+
+
+function clock(){
+    alert('como no vas a saber ? es tu cumpleaños(╬ﾟдﾟ)╭∩╮');
+}
+
+no6.addEventListener("click",function(){
+    var no6=document.getElementById('no6');
+    no6.href='bae4.html';
+    
+})
+
+
+no5.addEventListener("click",function(){
+    var no5=document.getElementById('no5');
+    no5.href='1.html';
+    
+})
+
+
+
+
+
+
+        
+
+        
+    
